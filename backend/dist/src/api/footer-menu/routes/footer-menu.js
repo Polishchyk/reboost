264982dfbda9446277b5fb825c20564a1babc1a1
Oleksandr Polishchuk <polishchyk.a.v@gmail.com>
2025-02-17 19:38:24 +0200

    feat: backend re-build admin panel

---
+++ backend/dist/src/api/footer-menu/routes/footer-menu.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * footer-menu router
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreRouter('api::footer-menu.footer-menu');
