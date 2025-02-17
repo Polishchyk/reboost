881feaca07cc228a3d0b7a7ddfc97cd36211f257
Oleksandr Polishchuk <polishchyk.a.v@gmail.com>
2025-02-17 20:13:38 +0200

    feat: backend re-build admin panel

---
+++ backend/dist/build/isNil-CB3NmwGu.js
@@ -0,0 +1,1 @@
+import{gl as r}from"./strapi-DyxV3B0J.js";function t(i){return i==null}var l=t;const s=r(l);export{s as i};
---
+++ backend/dist/build/useDragLayer-DD5duU_R.js
@@ -0,0 +1,1 @@
+import{fx as s,fy as c,r as o}from"./strapi-DyxV3B0J.js";function f(t){const e=s().getMonitor(),[a,r]=c(e,t);return o.useEffect(()=>e.subscribeToOffsetChange(r)),o.useEffect(()=>e.subscribeToStateChange(r)),a}export{f as u};
