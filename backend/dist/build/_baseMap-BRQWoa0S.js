57f66c7bd2ec395b9cdfc9042edb536cace3da9a
Oleksandr Polishchuk <polishchyk.a.v@gmail.com>
2025-03-23 19:01:40 +0200

    feat: build admin panel after changes

---
+++ backend/dist/build/_baseMap-BRQWoa0S.js
@@ -0,0 +1,1 @@
+import{_ as n}from"./_baseEach-h2WsT-Dn.js";import{b2 as p}from"./strapi-BPDDrlJX.js";var _=n,f=p;function m(a,s){var e=-1,r=f(a)?Array(a.length):[];return _(a,function(i,t,b){r[++e]=s(i,t,b)}),r}var o=m;export{o as _};
