f61f5672f66a513af3720890c5c2f030599ccae3
Oleksandr Polishchuk <polishchyk.a.v@gmail.com>
2025-10-27 11:31:30 +0200

    feat: change SMTP settings

--- backend/src/api/contact/content-types/contact/lifecycles.ts
+++ backend/src/api/contact/content-types/contact/lifecycles.ts
@@ -4,7 +4,7 @@ export default {
   async afterCreate(event) {
     const { result } = event;
     await emailService.sendEmail(
-      'sergey@reboost.ch',
+      'seo@reboost.ch',
       'New Contact Form Submission',
       `You received a new message from ${result.email}\n\nName: ${result.your_name}\n\nPhone: ${result.phone_number}\n\nMessage:\n${result.message}\n\n`,
       result.email
